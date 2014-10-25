8970742a395c1057621261e3d06041f966f3d50c
manahilov <manahilov@Boyans-Mac-mini.local>
2014-10-25 13:14:45 +0300

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -17,7 +17,7 @@
 {
     // put update code here
     timeSinceObstacle += delta;
-    if (timeSinceObstacle > 1.0f) {
+    if (timeSinceObstacle > 1.5f) {
         [self addObstacle];
         timeSinceObstacle = 0.0f;
     }
